25d14e82e3a77f731f03024e15e7548efb4d6826
EnnoHK <zokikame@hotmail.com>
2022-07-12 13:02:21 -0700

    Exercises and examples for using comparisons

---
+++ javascript.js
@@ -0,0 +1,69 @@
+//Variable declaration
+let a = 10
+let b = 7 * a
+
+const max = 57
+let actual = max - 13
+let percentage = actual / max
+
+//Print to console 
+console.log(a)
+console.log(a * 9)
+console.log(b)
+
+console.log(percentage)
+
+console.log(23 + 97)
+console.log(1 + 2 + 3 + 4 + 5 + 6)
+console.log((4 + 6 + 9) / 77)
+
+//new lesson (Strings)
+//Variable decleration
+const string = 'This is a string created for testing purposes';
+const sgl = 'Single quotes.';
+const dbl = "Double quotes";
+const bigmouth = 'I\'ve got no right to take my place...';
+const name = 'Jacks';
+const greeting = `Hello, ${name}`;
+const one = 'Hello, ';
+const two = 'how are you?';
+const joined = `${one}${two}`;
+console.log(joined); // "Hello, how are you?"
+console.log(greeting); // "Hello, Chris"
+console.log(bigmouth);
+console.log(sgl);
+console.log(dbl);
+
+console.log(string)
+
+// const badString1 = This is a test;
+// const badString2 = 'This is a test;
+// const badString3 = This is a test';
+
+//new lesson (Conditionals)
+console.log("---------------------------------------")
+//variable declaration
+
+
+console.log(2 > 1);  // true (correct)
+console.log(2 == 1); // false (wrong)
+console.log(2 != 1); // true (correct)
+//String comparisons
+console.log('Z' > 'A'); // true
+console.log('Glow' > 'Glee'); // true
+console.log('Bee' > 'Be'); // true
+
+console.log('Z' > 'A'); // true
+console.log('Glow' > 'Glee'); // true
+console.log('Bee' > 'Be'); // true
+
+//comparisons of different types
+console.log('2' > 1); // true, string '2' becomes a number 2
+console.log('01' == 1); // true, string '01' becomes a number 1
+
+console.log(true == 1); // true
+console.log(false == 0); // true
+
+//Comparison with null and undefined
+console.log(null === undefined); // false
+console.log(null == undefined); // true
---
+++ secondscript.js
@@ -0,0 +1,2 @@
+//https://www.theodinproject.com/lessons/foundations-fundamentals-part-2
+//https://javascript.info/logical-operators
